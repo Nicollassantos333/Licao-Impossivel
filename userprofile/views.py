527c34f630e606f07019a61ce31f1eaa4fdf75df
laloijovens3-dotcom <laloijovens3@gmail.com>
2026-06-19 08:33:51 -0300

    teste de commit

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+# Abra o userprofile/views.py e mude de:
+def home(request):
+    return render(request, 'index.html')
 
-# Create your views here.
+# Para:
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
